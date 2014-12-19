89007afe27ed25796640c849a4fa9d47f5cc7786
sparkCLuster <sparkCluster@usmc>
2014-12-19 15:15:16 +0000

    upgrade spark to 1.1.0

--- app/utils/SparkSQL.scala
+++ app/utils/SparkSQL.scala
@@ -10,11 +10,13 @@ case class WordCount(word: String, count: Int)
 object SparkSQL {
 
   def runQuery (query:String, day:String,datetime:String) {
+     println("Starting query ...... " )
     val logFile = "s3n://usmc-log-parsed/"+day+"/"+datetime+".json.gz" // Should be some file on your system
-    val driverHost = "localhost"
-    val conf = new SparkConf(false) // skip loading external settings
-      .setMaster("local[4]") // run locally with enough threads
-      .setAppName("firstSparkApp")
+    println("s3 file:"+logFile)
+    val driverHost = "10.0.3.50"
+    val conf = new SparkConf(true) // skip loading external settings
+      .setMaster("spark://10.0.3.50:7077") // run locally with enough threads
+      .setAppName("SparkPlay")
       .set("spark.logConf", "true")
       .set("spark.driver.host", s"$driverHost")
     val sc = new SparkContext(conf)
@@ -24,7 +26,7 @@ object SparkSQL {
 
     val sTable = "log_test"
     val sTableComodin = "%TABLE%"
-    logData.registerAsTable(sTable)
+    logData.registerTempTable(sTable)
 
     
     
